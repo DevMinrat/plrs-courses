f16187d97ceacbfd58109ed6a0092b77640b854f
andrey.koyro <andrey.koyro@objectstyle.com>
2023-11-07 21:12:04 +0100

    init project, add logger dependency

---
+++ src/main/java/com/plrs/coursesinfo/cli/CourseRetriever.java
@@ -0,0 +1,28 @@
+package com.plrs.coursesinfo.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String[] args) {
+        LOG.info("Application is starting");
+        if (args.length == 0) {
+            LOG.warn("Please provide an author name as firs argument");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            LOG.error("Unexpected error", e);
+        }
+
+    }
+
+    private static void retrieveCourses(String authorId) {
+        LOG.info("Retrieving courses for author '{}'", authorId);
+    }
+
+}
